abe7ade46284b889911bdb7d8196f8d7c4052f2e
TakakiLeo <leonardo-takaki@hotmail.com>
2023-05-02 19:54:42 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import ArticleList from "./components/ArticleList/ArticleList";
+import Header from "./components/Header/Header";
+import Form from "./components/ToggleTheme/Form/Form";
+
+const App = () => {
+  const [user, setUser] = useState();
+  const hasUser = Boolean(user);
+  return (
+    <div className="h-screen">
+      <Header user={user} />
+      {hasUser && <ArticleList />}
+      {hasUser || <Form onSubmit={setUser} />}
+    </div>
+  );
+};
+export default App;
---
+++ src/components/ToggleTheme/Form/Form.jsx
@@ -0,0 +1,30 @@
+export default function Form({ onSubmit }) {
+  const safeSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const name = event.target[0].value;
+    const email = event.target[1].value;
+    onSubmit({ name, email });
+  };
+  return (
+    <form
+      onSubmit={safeSubmit}
+      className="h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5">
+      <input
+        className="alura-input"
+        type="text"
+        required
+        placeholder="Insira seu nome..."
+      />
+      <input
+        className="alura-input"
+        type="text"
+        required
+        placeholder="Insira seu e-mail..."
+      />
+      <button className="alura-button" type="submit">
+        Seguir
+      </button>
+    </form>
+  );
+}
